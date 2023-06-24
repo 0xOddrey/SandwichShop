1b0041e894b29e78309e1ff04a5f310999af703e
0xOddrey <97917541+0xOddrey@users.noreply.github.com>
2023-06-24 14:29:42 -0500

    updates

--- sandwich-shop/pages/token/meta/[tokenId].tsx
+++ sandwich-shop/pages/token/meta/[tokenId].tsx
@@ -1,14 +1,16 @@
 import { useRouter } from 'next/router'
-import { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
  
 export default function Page() {
   const [tokenId, setTokenId] = useState("")
 
-  const router = useRouter()
+  const router = useRouter();
   useEffect(() => {
-    const token = router.query.tokenId
-    setTokenId(token as string)
-  }, [router.query.tokenId])
+      if (router.isReady) {
+        console.log(router.query);
+        setTokenId(router.query.tokenId as string)
+      }
+    }, [router.isReady, router.query]);
 
   if (!tokenId) return <div>Loading...</div>
   return (
