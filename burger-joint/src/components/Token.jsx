d7976693e9fa0bcf6837d91ad67fed9aa20a5d06
0xOddrey <97917541+0xOddrey@users.noreply.github.com>
2023-06-24 23:32:56 -0500

    updates

--- burger-joint/src/components/Token.jsx
+++ burger-joint/src/components/Token.jsx
@@ -1,13 +1,11 @@
-import { useParams } from "react-router-dom";
-
 
 export default function Token() {
-    const params = useParams();
+
 
 
     return (
         <div>
-            <h1>Token {params.tokenId}</h1>
+            <h1>Token </h1>
         </div>
         
     )
