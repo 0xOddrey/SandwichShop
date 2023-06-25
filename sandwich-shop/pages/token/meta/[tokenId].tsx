2a06f582e6017cc7ea6dc906067f52ace6f908cd
0xOddrey <97917541+0xOddrey@users.noreply.github.com>
2023-06-24 21:28:40 -0500

    updates

--- sandwich-shop/pages/token/meta/[tokenId].tsx
+++ sandwich-shop/pages/token/meta/[tokenId].tsx
@@ -4,29 +4,16 @@ import styles from "../styles/Home.module.css";
 
  
 export default function Page() {
-  const address = useAddress();
 
   const {
     contract 
   } = useContract('0x05D55A89De2680b194db09be1e6A7C30ee144193');
 
-  const {
-    data,
-    isLoading 
-  } = useOwnedNFTs(contract, address)
 
-  if (!data || isLoading) {
-    return (
-      <>
-        <h1>Loading...</h1>
-      </>
-    )
-  }
-  console.log(data[0])
   return (
       <>
         <h1>This is the token</h1>
-        <p>Token:  {data[0].owner} </p>
+        <p>Token:  {contract?.chainId} </p>
       </>
     )
 }
