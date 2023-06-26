03ba4e79345c40db67058b17361c08046ee2d156
0xOddrey <97917541+0xOddrey@users.noreply.github.com>
2023-06-25 19:05:10 -0500

    updates

--- burger-joint/src/Components/SmartWallets.ts
+++ burger-joint/src/Components/SmartWallets.ts
@@ -1,28 +1,37 @@
-import { ethers } from "ethers";
-import { NFT } from "@thirdweb-dev/react";
+
+
+import { NFT } from "@thirdweb-dev/sdk";
+
 import {
-  TWAPI_KEY,
   factoryAddress,
   activeChain,
   NFTDROP_ADDRESS,
   IMPLEMENTATION_ADDRESS,
-  EDITIONDROP_ADDRESS
 } from "../utils/constants";
-
+import { Network, Alchemy } from "alchemy-sdk";
 import Web3 from 'web3';
 var web3js = new Web3(new Web3.providers.HttpProvider('https://polygon-mumbai.g.alchemy.com/v2/mkWadI2ogZ3dw2tsjF3o7hN3h3hlZXaU'));
-import { Network, Alchemy } from "alchemy-sdk";
 
-
-
-const abi: any[] = [
-    {"inputs":[{"internalType":"address","name":"implementation","type":"address"},{"internalType":"uint256","name":"chainId","type":"uint256"},{"internalType":"address","name":"tokenContract","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"},{"internalType":"uint256","name":"salt","type":"uint256"}],"name":"account","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"}
-]
 
 export default async function smartWallet(token: NFT) {
     let nftList: any[] = []
-    let contractWeb3 = new web3js.eth.Contract(abi, factoryAddress);
-    let smartWalletAddress = await contractWeb3.methods.account(
+    var contractWeb3 = new web3js.eth.Contract([
+            {"inputs":[
+                {"internalType":"address","name":"implementation","type":"address"},
+                {"internalType":"uint256","name":"chainId","type":"uint256"},
+                {"internalType":"address","name":"tokenContract","type":"address"},
+                {"internalType":"uint256","name":"tokenId","type":"uint256"},
+                {"internalType":"uint256","name":"salt","type":"uint256"}
+                    ],
+            "name":"account",
+            "outputs":[
+                {"internalType":"address","name":"","type":"address"}
+                ],
+            "stateMutability":"view","type":"function"}
+        ], factoryAddress);
+    console.log(contractWeb3)
+  
+    const smartWalletAddress = await contractWeb3.methods.account(
       IMPLEMENTATION_ADDRESS,
       activeChain.chainId,
       NFTDROP_ADDRESS,
@@ -36,7 +45,7 @@ export default async function smartWallet(token: NFT) {
     };
     
     const alchemy = new Alchemy(settings);
-    const nfts = await alchemy.nft.getNftsForOwner(smartWalletAddress)
+    const nfts = await alchemy.nft.getNftsForOwner(smartWalletAddress as unknown as string)
     nftList.push(nfts)
     return [smartWalletAddress, nftList];
   }
